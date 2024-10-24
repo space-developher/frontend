663c3c0af4aa02caeb62b20e128ceffaf6e09e5f
조덕행 <space.developher@gmail.com>
2024-10-24 10:30:22 +0900

    Demo 기본값 설정

--- demo/src/configs/sections/index.ts
+++ demo/src/configs/sections/index.ts
@@ -4,9 +4,9 @@ import { demoEntitiesSections } from "./entities";
 import { demoLovelaceSections } from "./lovelace";
 
 export const demoSections: DemoConfig = {
-  authorName: "Home Assistant",
+  authorName: "Accordion",
   authorUrl: "https://github.com/home-assistant/frontend/",
-  name: "Home Demo",
+  name: "Accordion",
   description: demoLovelaceDescription,
   lovelace: demoLovelaceSections,
   entities: demoEntitiesSections,
